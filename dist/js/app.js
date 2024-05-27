9558a739524bf1e96743c9102847c6a2b076369c
fagner <fagner.gomes@viacometa.com.br>
2024-05-27 13:25:02 -0400

    [FEAT] Aula 5: TSC e StrictNullChecks

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,12 @@
 import { NegociacaoController } from "./controllers/NegociacaoController.js";
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-});
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+}
+else {
+    throw Error('Não foi possivel inicializar a aplicação, verifique se o formulário existe');
+}
